a022e5912445966dff5b1e7c5a178feae51c4b0b
Juan Carlos <juancarlospaco@gmail.com>
2024-11-05 15:46:08 -0300

    feat: Check if token is a JWT

--- supabase_functions/utils.py
+++ supabase_functions/utils.py
@@ -17,3 +17,27 @@ def is_valid_str_arg(target: str) -> bool:
 
 def is_http_url(url: str) -> bool:
     return urlparse(url).scheme in {"https", "http"}
+
+
+def is_valid_jwt(value: str) -> bool:
+    """Checks if value looks like a JWT, does not do any extra parsing."""
+    if not isinstance(value, str):
+        return False
+
+    # Remove trailing whitespaces if any.
+    value = value.strip()
+
+    # Remove "Bearer " prefix if any.
+    if value.startswith("Bearer "):
+        value = value[7:]
+
+    # Valid JWT must have 2 dots (Header.Paylod.Signature)
+    if value.count(".") != 2:
+        return False
+
+    okcr = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789_-."
+    for char in value:
+        if not char in okcr:
+            return False
+
+    return True
